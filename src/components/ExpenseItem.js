2db7ebaf4684c881be1bc3fabfe3d430da7a2c0b
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-24 16:37:53 +0530

    create expense component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){  // react component must have one root return element
+    return(
+        <div>
+            <div>Oct 25 2001</div>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$345.45</div>
+            </div>
+        </div> 
+    )
+}
+
+export default ExpenseItem
