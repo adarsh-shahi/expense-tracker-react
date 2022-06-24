0c74c4475e018e5d863008e961a0c2f1e5e5c080
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-24 18:31:07 +0530

    add seperate compo for expense date and a bit styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 
 function ExpenseItem(props){  // react component must have one root return element
-    const expenseDate = props.date.toISOString()  // props.data -> returns an object
     const expenseTitle = props.title
     const expenseAmount = props.amount
 
-    return(   // in {} in can run js expressions we have added dynamic values here
+    return(   // in {} you can run js expressions we have added dynamic values here
         <div className='expense-item'>
-            <div>{expenseDate}</div>  
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>${expenseAmount}</div>
