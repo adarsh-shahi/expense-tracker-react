9b6149b828a5d74ad8728f8a7d87f6f69a25083b
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-24 20:49:40 +0530

    add wrapper card componenet

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
+import Card from './Card'
 
 function ExpenseItem(props){  // react component must have one root return element
     const expenseTitle = props.title
     const expenseAmount = props.amount
 
     return(   // in {} you can run js expressions we have added dynamic values here
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>${expenseAmount}</div>
             </div>
-        </div> 
+        </Card> 
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import App from './App';
 
 
 //the contents of div element (id = root) is being replaced by App
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
  
