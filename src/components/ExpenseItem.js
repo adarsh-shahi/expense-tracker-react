6f448a7a48b7deb570158ed93277081e316be354
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-24 18:06:31 +0530

    use props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css'
 
-function ExpenseItem(){  // react component must have one root return element
-    const expenseDate = new Date(2022, 5, 24);  // returns an object
-    const expenseTitle = 'Car Insurance'
-    const expenseAmount = 345.45
+function ExpenseItem(props){  // react component must have one root return element
+    const expenseDate = props.date.toISOString()  // props.data -> returns an object
+    const expenseTitle = props.title
+    const expenseAmount = props.amount
 
     return(   // in {} in can run js expressions we have added dynamic values here
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>  
+            <div>{expenseDate}</div>  
             <div className='expense-item__description'>
                 <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>${expenseAmount}</div>
