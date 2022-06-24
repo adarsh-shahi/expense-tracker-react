4fae2e7f3f3f81ffabf8f8d0c2ca362a22435c81
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-24 17:02:08 +0530

    add dynamic values

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem(){  // react component must have one root return element
-    return(
+    const expenseDate = new Date(2022, 5, 24);  // returns an object
+    const expenseTitle = 'Car Insurance'
+    const expenseAmount = 345.45
+
+    return(   // in {} in can run js expressions we have added dynamic values here
         <div className='expense-item'>
-            <div>Oct 25 2001</div>
+            <div>{expenseDate.toISOString()}</div>  
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$345.45</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expenseAmount}</div>
             </div>
         </div> 
     )
