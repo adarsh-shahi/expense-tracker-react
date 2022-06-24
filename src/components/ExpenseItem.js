0d75f3bd791e51017e3a6b8df2deb2a935a76a82
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-24 16:47:46 +0530

    add styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css'
+
 function ExpenseItem(){  // react component must have one root return element
     return(
-        <div>
+        <div className='expense-item'>
             <div>Oct 25 2001</div>
-            <div>
+            <div className='expense-item__description'>
                 <h2>Car Insurance</h2>
-                <div>$345.45</div>
+                <div className='expense-item__price'>$345.45</div>
             </div>
         </div> 
     )
