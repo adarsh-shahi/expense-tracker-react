f29699a33ddff349de81a00e38f876ec8cd43b3e
adarsh-shahi <adarshshahi1009@gmail.com>
2022-06-25 13:50:59 +0530

    add filter year in expenses

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,27 +4,27 @@ import ExpenseDate from './ExpenseDate'
 import Card from '../UI/Card'
 
 function ExpenseItem(props){  // react component must have one root return element
-     const [title, setTitle] = useState(props.title);  // setting inital value of props.title to title variable this is dobe aonly once at the begining
+  //   const [title, setTitle] = useState(props.title);  // setting inital value of props.title to title variable this is dobe aonly once at the begining
      // type of react hook must be called directly inside a component
      //  useState returns array of exactly two elements
      // 1 element is current state value
      // 2 element is function for updating that value
-     console.log(title);
   
-    const expenseAmount = props.amount
+  
+  //  const expenseAmount = props.amount
 
-    const clickHandler = () => {
-        setTitle('OMKAY') //calling this state updating function not just to change the value (title) but also to rerender (rerun) this component (ExpenseItem) function 
-    }
+    // const clickHandler = () => {
+    //     setTitle('OMKAY') //calling this state updating function not just to change the value (title) but also to rerender (rerun) this component (ExpenseItem) function 
+    // }
 
     return(   // in {} you can run js expressions we have added dynamic values here
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
+            {/* <button onClick={clickHandler}>Change Title</button> */}
         </Card> 
     )
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,21 @@
 import './Expenses.css'
 import ExpenseItem from './ExpenseItem'
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
+import { useState } from 'react'
 
 function Expenses(props) {
+
+    const [filteredYear, setFilteredYear] = useState('2020')
+
+    const filterChangeHandler = (selectedYear) => [
+        setFilteredYear(selectedYear)
+    ]
+
  
     return(
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             {/* title="ps5 "  we could have used hardcored value*/ }
             <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
             <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
